1890fd299b5979507e8c95cc114bc5e24278ca8c
sy4mss <agungngrahaa@gmail.com>
2025-12-26 18:51:26 +0700

    pages

--- app.py
+++ app.py
@@ -1,9 +1,9 @@
 import streamlit as st
 
 # Define the pages
-main_page = st.Page("main_page.py", title="Main Page", icon="🎈")
-page_2 = st.Page("page_2.py", title="Page 2", icon="❄️")
-page_3 = st.Page("page_3.py", title="Page 3", icon="🎉")
+main_page = st.Page("pages/main_page.py", title="Main Page", icon="🎈")
+page_2 = st.Page("pages/page_2.py", title="Page 2", icon="❄️")
+page_3 = st.Page("pages/page_3.py", title="Page 3", icon="🎉")
 
 # Set up navigation
 pg = st.navigation([main_page, page_2, page_3])
